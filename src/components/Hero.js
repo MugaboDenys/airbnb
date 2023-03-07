09c7939a6682e80ee6ebf31473a82dc8d534dd43
MugaboDenys <mugabodan2020@gmail.com>
2023-03-07 17:37:07 +0200

    fixed img in deployment

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
         <section className="hero">
             <img src="../images/photo-grid.png" className="hero--photo" alt="" />
             <h1 className="hero--header">Online Experiences</h1>
-            <p className="hero--text">Join unique interactive activities led by 
+            <p className="hero--text">Join unique interractive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
         </section>
     )
